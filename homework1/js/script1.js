ed9e2f791bd3f6e13ed95c36779dac53b06c666a
HrytsivV <162177824+HrytsivV@users.noreply.github.com>
2024-05-06 17:54:13 +0100

    Create script1.js

---
+++ homework1/js/script1.js
@@ -0,0 +1,109 @@
+//** 1 */
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах 1, 0, -3.
+
+//const min = (0);
+//if (min >= 0) {
+ //   console.log(true);
+//}
+//else if (min <= 0) {
+ //   console.log(false);
+//}
+
+//** 2 */
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах 'test',"qwerty", true
+
+//let variable = "qwerty";
+//let result = (variable === "test") ? true : true;
+//console.log(result);
+
+//** 3 */
+// Якщо змінна більше 10 - відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13.
+
+//let min = 1 ;
+//if (min >= 10) {
+ //   console.log( 10 - 5);
+//}
+//else if (min <= 10) {
+ //   console.log(10 + 5);
+//}
+
+//** 4 */
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+//let mounth;
+//const sub = 5;
+
+//switch (sub) {
+  //  case 1:
+    //    mounth = 'January';
+      //  console.log(mounth);
+    //break;
+    
+//    case 2:
+  //  mounth = 'February';
+    //    console.log(mounth);
+      //  break;
+   
+   // case 3:
+   // mounth = 'March';
+     //   console.log(mounth);
+       // break;
+    
+   // case 4:
+   // mounth = 'April';
+     //   console.log(mounth);
+       // break;
+    
+//    case 5:
+//    mounth = 'May';
+//        console.log(mounth);
+//        break;
+
+//case 6:
+ //   mounth = 'June';
+  //      console.log(mounth);
+   //     break;
+    
+//case 7:
+  //  mounth = 'July';
+    //    console.log(mounth);
+      //  break;
+   
+  //  case 8:
+    //mounth = 'Augest';
+      //  console.log(mounth);
+        //break;
+   
+
+  //  case 9:
+    //mounth = 'September';
+      //  console.log(mounth);
+        //break;
+  
+  //  case 10:
+    //mounth = 'October';
+      //  console.log(mounth);
+        //break;
+    
+    //case 11:
+  //  mounth = 'November';
+    //    console.log(mounth);
+      //  break;
+    
+   // case 12:
+   // mounth =  'December';
+//        console.log(mounth);
+//        break;
+//}
+
+//** 5 */
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+//const number = (326);
+//let value = 0;
+//console.log( 3 + 2 + 6);
